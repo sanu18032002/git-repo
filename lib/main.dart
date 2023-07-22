f1efd0c59522193c7d60d0bc2f386aa7b22ed050
Akshay_Kumar4 <sanu18032002@gmail.com>
2023-07-22 04:12:37 +0000

    nbnb

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,46 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return Scaffold(
+      //backgroundColor: Colors.blue,
+      appBar: AppBar(
+          backgroundColor: Colors.blue.shade900,
+          title: Center(
+            child: Text('Ask Me Anything'),
+          )),
+      body: Ball(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
 
   @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int dice = 1;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
+    return Center(
+        child: TextButton(
+      onPressed: () {
+        setState(() {
+          dice = Random().nextInt(6) + 1;
+        });
+      },
+      child: Image.asset('images/d$dice.jpg'),
+    ));
   }
 }
